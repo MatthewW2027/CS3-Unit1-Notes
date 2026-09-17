5356d74f5bb58e04059e8e4694404c57bdf21cea
Matthew <149590049+MatthewW2027@users.noreply.github.com>
2026-09-17 16:34:29 +0000

    day 1 notes

--- main.py
+++ main.py
@@ -3,4 +3,20 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+        main()
+
+# variable name example: more_than_one_word 
+# NOT ALLOWED TO START VARIABLE NAMES LIKE THIS
+# starting with a number, special char, keywords: and, if, ture, false 
+# careful: int, list, str - can be overwritten 
+
+# numbers: 
+    # int: x = 500 
+    # floats: x = 500.1 
+    # complex: x = 30j 
+    # x = 75 float(75) - used to change a varibale type 
+        # example: grade = 92.875 
+        # print("your grade is: ")
+        #print (int(grade))
+    # division automatically gives you a float and if you wanted an int you would have to cast it 
+    
